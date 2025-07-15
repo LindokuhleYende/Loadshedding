dc555a25abb765b6b7da56aead9999799a69aa00
Lindokuhle <yendelindokuhle2@gmail.com>
2025-07-15 13:08:49 +0200

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/signup",
+        "https://loadshedding.onrender.com/signup",
         {
           ...inputValue,
         },
